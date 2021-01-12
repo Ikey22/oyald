be8bd3e88dafeb9f8e7b88e6646a6cd0fa8155af
danroyal001 <danroyaleffiong@gmail.com>
2021-01-12 17:02:46 +0100

    Tue Jan 12th 2020 - 5:02:43pm

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
-window.addEventListener('load', function (){
-    return window.scrollTo(10, 0);
-})
+
+var sticky = document.querySelector('nav.sticky');
+
+var makeHeaderSticky = function(){
+    if (sticky.scrollTop >= 150){
+        sticky.classList.add('fixed')
+    } else if (sticky.classList.contains('fixed')) {
+        sticky.classList.remove('fixed')
+    }
+}
+
+document.body.addEventListener('scroll', makeHeaderSticky);
+document.body.addEventListener('resize', makeHeaderSticky);
+document.body.addEventListener('load', makeHeaderSticky);
