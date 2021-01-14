dd21dee3e83bb166e2c43566f7d2930f6a3e6397
danroyal001 <danroyaleffiong@gmail.com>
2021-01-14 14:13:41 +0100

    Thu Jan 14th 2020 - 2:13:39pm

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import VueMaterial from 'vue-material'
-
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-import '@/assets/css/fontawesome.min.css'
-
-Vue.use(VueMaterial)
-
-Vue.material.router.linkActiveClass = 'primary-link'
 
 Vue.config.productionTip = false
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,87 +7,9 @@ export default new Vuex.Store({
   state: {
     primaryColor: "#25ACCA",
     appName: "PayJaro",
-    appbarTitle: "PayJaro",
-    mainSidebarOpen: false,
-    currentUser: true,
-    sidebarItems: [
-      {
-        title: "Get started",
-        icon: "home",
-        to: "/home",
-        tooltip: "PayJaro Home Page"
-      },
-      {
-        title: "Help & Support",
-        icon: "chat_bubble_outline",
-        to: "/help-and-support",
-        tooltip: "Get help/support"
-      },
-      {
-        title: "Terms of Service",
-        icon: "chrome_reader_mode",
-        to: "/terms-of-service",
-        tooltip: "Terms and conditions / Privay policy"
-      },
-      {
-        title: "Report a problem",
-        icon: "warning",
-        to: "/report-a-problem",
-        tooltip: "Report a bug"
-      },
-      {
-        title: "FAQs",
-        icon: "info_outline",
-        tooltip: "Frequently asked questions"
-      },
-      {
-        title: "My QR code",
-        icon: "qr_code_scanner",
-        tooltip: "Your Unique QR code"
-      },
-      {
-        title: "Logout",
-        icon: "exit_to_app",
-        tooltip: "Logout now"
-      }
-    ],
-    authUserLinks: [
-      {
-        title: "Overview",
-        icon: "dashboard",
-        tooltip: "Overview",
-        to: "/home/overview"
-      },
-      {
-        title: "Wallet",
-        icon: "account_balance_wallet",
-        tooltip: "my wallet balance / transaction history",
-        to: "/home/wallet"
-      },
-      {
-        title: "Rewards",
-        icon: "star",
-        tooltip: "My Rewards / Cashback",
-        to: "/home/rewards"
-      },
-      {
-        title: "Profile",
-        icon: "account_circle",
-        tooltip: "My Profile",
-        to: "/home/profile"
-      }
-    ],
-    mdPersistent: "mini",
-    showSendMoneyDialog: false
+    appbarTitle: "PayJaro"
   },
   mutations: {
-    setAppbarTitle(state, title){
-      state.appbarTitle = title
-    },
-
-    showSendDialog(state){
-      state.showSendMoneyDialog = true;
-    }
   },
   actions: {
   },
