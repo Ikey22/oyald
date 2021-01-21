a13c0de75d4bd8d0f8f6a7c4f03e3b5ccdfc9554
danroyal001 <danroyaleffiong@gmail.com>
2021-01-21 11:05:24 +0100

    Thu Jan 21st 2020 - 11:05:22am

---
+++ src/components/NetworkError.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-100 container-fluid d-flex align-items-center justify-content-center flex-column">
+      <br />
+      <p class="h1 font-weight-bold d-inline-block">
+        <u class="d-inline-block text-danger">Network Error</u>
+        <br />
+        <b-iconstack aria-label="" class="center text-danger">
+          <b-icon icon="link" />
+          <b-icon icon="slash" />
+        </b-iconstack>
+      </p>
+      <br />
+      <br />
+      <p class="h3 text-center w-100 text-black font-weight-bold">Oops, It appears your network connection is bad, please try again later</p>
+      <r />
+      <p class="h5 w-100 text-black text-center">Or, <b-button to="/home" class="bg-primary-color"> click here </b-button> to return to the Home Page</p>
+      <br />
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NetworkError"
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import VueRouter from 'vue-router';
 import HomePage from '@/views/HomePage.vue';
 import LandingPage from '@/views/LandingPage.vue';
 import NotFound404 from '@/views/NotFound404.vue';
-import Preloader from '@/components/Preloader.vue';
+import PreLoader from '@/components/Preloader.vue';
+import NetworkError from '@/components/NetworkError.vue';
 
 Vue.use(VueRouter);
 
@@ -14,47 +15,73 @@ const routes = [
     name: 'LandingPage',
     component: LandingPage
   },
+
   {
     path: '/home',
     alias: '/home-page',
     name: 'HomePage',
     component: HomePage
   },
+
   {
     path: '/about',
     alias: '/about-page',
     name: 'AboutPage',
-    component: () => import(/* webpackChunkName: "about", webpackPrefetch: true */ '@/views/AboutPage.vue')
+    component: () => ({
+      timeout: 60000,
+      delay: 1,
+      loading: PreLoader,
+      error: NetworkError,
+      component: import(/* webpackChunkName: "about", webpackPrefetch: true */ '@/views/AboutPage.vue')
+    })
   },
+
   {
     path: '/initiatives',
     alias: '/initiatives-page',
     name: 'InitiativesPage',
-    component: () => import(/* webpackChunkName: "about", webpackPrefetch: true */ '@/views/InitiativesPage.vue')
+    component: () => ({
+      timeout: 60000,
+      delay: 1,
+      loading: PreLoader, 
+      error: NetworkError,
+      component: import(/* webpackChunkName: "about", webpackPrefetch: true */ '@/views/InitiativesPage.vue')
+    })
   },
+
   {
     path: '/membership',
     alias: '/join-oyald',
     name: 'JoinOyald',
-    component: () => import(/* webpackChunkName: "join-oyald", webpackPrefetch: true */ '@/views/JoinOyald.vue')
+    component: () => ({
+      timeout: 60000,
+      delay: 1,
+      loading: PreLoader, 
+      error: NetworkError,
+      component: import(/* webpackChunkName: "join-oyald" */ '@/views/JoinOyald.vue')
+    })
   },
+
   {
     path: "/team",
     alias: "/our-team",
     name: "OurTeam",
     component: () => import(/* webpackChunkName: "our-team", webpackPrefetch: true */ '@/views/OurTeam.vue')
   },
+
   {
     path: "/contact",
     alias: "/contact-us",
     name: "ContactUs",
-    component: () => import(/* webpakChunkName: "contact-us", webpackPrefetch: true */ '@/views/ContactUs.vue')
+    component: () => import(/* webpakChunkName: "contact-us" */ '@/views/ContactUs.vue')
   },
+
   {
     path: "__admin",
     name: "Admin",
     component: () => import(/*  webpackChunkName: "admin" */ '@/views/Admin.vue')
   },
+
   {
     path: "*",
     name: "NotFound404",
