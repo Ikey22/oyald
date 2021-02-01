a01a65d4a2f1bb6efe8587e94a5269fc82cab417
danroyal001 <danroyaleffiong@gmail.com>
2021-02-01 16:01:55 +0100

    Mon Feb 1st 2021 - 4:01:51pm

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,30 +1,14 @@
 <template>
-  <b-overlay
-      variant="dark"
-      opacity=".95"
-      rounded="sm"
-      class="w-100 h-100 position-absolute"
-      style="top: 0px !important; left: 0px !important;"
-      >
-      <template #overlay>
-        <div
-          class="d-flex align-items-center"
-          >
-          <b-spinner small type="grow" variant="warning"></b-spinner>
-          <b-spinner type="grow" variant="warning"></b-spinner>
-          <b-spinner small type="grow" variant="warning"></b-spinner>
-          <br />
-          <span class="h3 w-100 text-center text-white font-weight-bold">Loading, lease wait...</span>
-        </div>
-      </template>
-
-      <div>&nbsp;</div>
-    </b-overlay>
+  <div class="w-100 h-100 d-flex align-items-center justify-content-center flex-column">
+    <br />
+    <b-spinner />
+    <br />
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: "Preloader"
 }
 </script>
 
