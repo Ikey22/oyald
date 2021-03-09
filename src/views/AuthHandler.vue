37f801efc52bd119574c61f7c5d3bfb43345182e
Daniel Effiong <danroyal001@pop-os.localdomain>
2021-03-09 09:57:27 +0100

    Tue Mar 9th 2021 - 9:57:19am - GMT+1

---
+++ src/views/AuthHandler.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="w-100 h-100 container-lg d-flex flex-column align-items-center justify-content-around">
+      <br />
+      <br />
+  </div>
+</template>
+
+<script>
+export default {
+    name: "AuthHandler",
+
+    mounted(){
+        const $this = this;
+        const query = this.$route.query;
+        const auth = this.$firebase.auth();
+
+        const mode = query.mode;
+        const lang = query.lang || "en";
+        const oobCode = query.oobCode;
+        const continueUrl = query.continueUrl;
+
+        switch (mode) {
+            case "resetPassword":
+                (() => {})();
+                break;
+
+            case "recoverEmail":
+                (() => {})();
+                break;
+
+            case "verifyEmail":
+                (() => {})();
+                break;
+        
+            default:
+                (() => {
+                    alert("Invalid action");
+                    $this.$router.push("/home-page")
+                })();
+                break;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
