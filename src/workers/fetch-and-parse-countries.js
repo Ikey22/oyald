23da2c2e8cc7c47c0f60bfbd8629986749f2a552
Danroyal001 <danroyaleffiong@gmail.com>
2021-05-21 08:04:13 +0100

    optimized transfer of countries from web worker fo ui thread

--- src/workers/fetch-and-parse-countries.js
+++ src/workers/fetch-and-parse-countries.js
@@ -1,35 +1,48 @@
 const $self = self;
 
-$self.onmessage = ({ data }) => fetch(data)// || '/country-stats-city.jon')
-           .then(res => res.json())
-           .then(json => {
+$self.onmessage = ({
+        data
+    }) => fetch(data) // || '/country-stats-city.jon')
+    .then(res => res.json())
+    .then(async json => {
 
-               const payload = JSON.parse(JSON.stringify(json));
+        const payload = JSON.parse(JSON.stringify(json));
 
-               const retVal = {
-                   status: 'success',
-                   payload
-               };
-               
-               console.trace("Ready to post");
-               // $self.postMessage(retVal);
-               $self.postMessage({status: "starting"});
-               retVal.payload.forEach(country => $self.postMessage({
-                   status: "success",
-                   value: country
-               }));
-               $self.postMessage({status:"ended"});
-               console.trace("after post message");
-               //console.trace(retVal);
-           })
-           .catch(err => {
-               const payload = `Unable to get list of countries, states and cities:  ${JSON.stringify(err)}`;
+        const retVal = {
+            status: 'success',
+            payload
+        };
 
-               $self.postMessage({
-                   status: 'error',
-                   payload
-               });
+        console.trace("Ready to post");
+        // $self.postMessage(retVal);
+        $self.postMessage({
+            status: "starting"
+        });
+        
+        for (const country of retVal.payload) {
 
-               console.trace(err);
+            $self.postMessage({
+                status: "success",
+                value: country
+            })
 
-               });
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        
+        $self.postMessage({
+            status: "ended"
+        });
+        console.trace("after post message");
+        //console.trace(retVal);
+    })
+    .catch(err => {
+        const payload = `Unable to get list of countries, states and cities:  ${JSON.stringify(err)}`;
+
+        $self.postMessage({
+            status: 'error',
+            payload
+        });
+
+        console.trace(err);
+
+    });
